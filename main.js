327728a0695d98fc94bdefbcf80af3bb6da093fd
DSchultzContractor <57688798+DSchultzContractor@users.noreply.github.com>
2020-02-04 13:47:45 -0700

    test

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ async function run() {
         spec: fs.createReadStream(path.resolve(process.cwd(), 'file.json')),
       },
       headers: {
-        'x-readme-version': `"1.0"`,
+        'x-readme-version': \\\"1.0.0\\\",
         'x-readme-source': 'github',
       },
       auth: { user: readmeKey },
